fa7dbf79f38f9272033381883d8b96e08a64ad97
Blue Poit Art Gallery Project <info@bluepointart.uk>
2025-04-23 22:35:01 +0100

    changed the rest of assets paths to CIDs. added _moveToVideo to see full video when played; added 'title' to config and modal

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { buildGallery } from '@your-org/art-modules';
 
-// your own config CID (pointing at a different JSON on IPFS)
-const CONFIG_CID = 'bafkreic4qb5hwznsoyx3yeijloeixsr4uwi6nykuyswd3j2s7dzqo7kk4q';
+const CONFIG_CID = 'bafkreifbkhbxhgfqxkim6ujs5a3afkd5632bztwkwqri4ybhpfr4cjed7y';
 const CONFIG_URL = `https://ipfs.io/ipfs/${CONFIG_CID}`;
+
+//const CONFIG_URL = "./exhibit_puno85_config.json";
+//const CONFIG_URL = "https://bafybeicmvcsqc6lai5ufwkw5qoyfzdk47fgnsrxmodye5zccuybnjsc35e.ipfs.w3s.link/exhibit_puno85_config.json";
 
 (async () => {
   const res = await fetch(CONFIG_URL);
   const cfg = await res.json();
   await buildGallery(cfg);
 })();
+//
