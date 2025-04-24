4f114608c1dc0d2ab04799833f7f720ff36dba2e
Blue Poit Art Gallery Project <info@bluepointart.uk>
2025-04-25 00:07:56 +0100

    fixed pointer on mobile

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { buildGallery } from '@your-org/art-modules';
 
-const CONFIG_CID = 'bafkreifbkhbxhgfqxkim6ujs5a3afkd5632bztwkwqri4ybhpfr4cjed7y';
-const CONFIG_URL = `https://ipfs.io/ipfs/${CONFIG_CID}`;
+//const CONFIG_CID = 'bafybeiacxiiqnajlgll6naaulp6ervnfte6kbp75hkhsj4gzpzz7wxze7m';
+//const CONFIG_URL = `https://ipfs.io/ipfs/${CONFIG_CID}`;
 
-//const CONFIG_URL = "./exhibit_puno85_config.json";
-//const CONFIG_URL = "https://bafybeicmvcsqc6lai5ufwkw5qoyfzdk47fgnsrxmodye5zccuybnjsc35e.ipfs.w3s.link/exhibit_puno85_config.json";
+//const CONFIG_URL = "./config_puno85/exhibit_puno85_config.json";
+const CONFIG_URL = "https://bafybeiacxiiqnajlgll6naaulp6ervnfte6kbp75hkhsj4gzpzz7wxze7m.ipfs.w3s.link/exhibit_puno85_config.json";
 
 (async () => {
   const res = await fetch(CONFIG_URL);
